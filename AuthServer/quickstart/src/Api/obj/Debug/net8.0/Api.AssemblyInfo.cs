c3b715695816d556583bf337e396f82406a2bebb
Ofir Adany <ofiradany@gmail.com>
2024-08-31 12:40:46 +0300

    starting from 0 the auth service

--- AuthServer/quickstart/src/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ AuthServer/quickstart/src/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d39d8349d35401ce3e9a18b0e570b53adbbe9b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70fa88ca111388be0452cc56ce37a79dbad9b5ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
